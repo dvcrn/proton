99e31aaae639fa4e894d9d824b5d3af827a02164
David Mohl <me@dave.cx>
2015-12-09 14:59:29 +0900

    added deep-merge and deep-merge-with helpers

--- src/proton/lib/helpers.cljs
+++ src/proton/lib/helpers.cljs
@@ -61,3 +61,24 @@
 (defn process->html [steps]
   (let [steps-html (map #(str "<tr><td class='process-step'>" (get % 0) "</td><td class='process-status'>" (get % 1) "</td></tr>") steps)]
     (str "<h2>Welcome to proton<h2><table>" (string/join " " steps-html) "</table>")))
+
+(defn deep-merge
+  "Deeply merges maps so that nested maps are combined rather than replaced.
+  For example:
+  (deep-merge {:foo {:bar :baz}} {:foo {:fuzz :buzz}})
+  ;;=> {:foo {:bar :baz, :fuzz :buzz}}
+  ;; contrast with clojure.core/merge
+  (merge {:foo {:bar :baz}} {:foo {:fuzz :buzz}})
+  ;;=> {:foo {:fuzz :quzz}} ; note how last value for :foo wins"
+  [& vs]
+  (if (every? map? vs)
+    (apply merge-with deep-merge vs)
+    (last vs)))
+
+(defn deep-merge-with
+  "Deeply merges like `deep-merge`, but uses `f` to produce a value from the
+  conflicting values for a key in multiple maps."
+  [f & vs]
+  (if (every? map? vs)
+    (apply merge-with (partial deep-merge-with f) vs)
+    (apply f vs)))
