9ecf5355ba4afaae7059cada99a09979d37ecc1a
Thomas Heller <info@zilence.net>
2015-12-09 15:24:13 +0100

    update

--- src/dev/build.clj
+++ src/dev/build.clj
@@ -1,6 +1,7 @@
 (ns build
   (:require [shadow.cljs.build :as cljs]
             [shadow.cljs.umd :as umd]
+            [shadow.devtools.server :as devtools]
             [clojure.java.io :as io]))
 
 (defn- plugin-setup []
@@ -24,6 +25,24 @@
 (defn dev []
   (-> (plugin-setup)
       (cljs/watch-and-repeat!
+        (fn [state modified]
+          (-> state
+              (cljs/compile-modules)
+              (umd/flush-unoptimized-module))))))
+
+(defn dev-once []
+  (-> (plugin-setup)
+      (cljs/compile-modules)
+      (umd/flush-unoptimized-module))
+  :done)
+
+(defn dev-repl []
+  (-> (plugin-setup)
+      (devtools/start-loop
+        {:before-load 'proton.core/stop
+         :after-load 'proton.core/start
+         :reload-with-state true
+         :node-eval true}
         (fn [state modified]
           (-> state
               (cljs/compile-modules)
