1e8b5c32468ca9140cd61d5a0ca5a1d0e2754ac3
geksilla <buzhordenis@gmail.com>
2015-12-11 03:37:51 +0200

    Added mode specific keybindings.

--- src/cljs/proton/layers/lang/clojure/core.cljs
+++ src/cljs/proton/layers/lang/clojure/core.cljs
@@ -1,8 +1,19 @@
 (ns proton.layers.lang.clojure.core
+  (:require [proton.lib.mode :as mode :refer [define-mode define-keybindings]]
+            [proton.lib.atom :as atom-env :refer [set-grammar]])
   (:use [proton.layers.base :only [init-layer! get-initial-config get-keybindings get-packages get-keymaps]]))
 
 (defmethod init-layer! :lang/clojure
   [_ config]
+  ()
+  (mode/define-mode :clojure
+     :atom-grammars ["Clojure"]
+     :file-extensions [#"\.proton$"]
+     :init (fn []
+            (atom-env/set-grammar "Clojure")))
+  (mode/define-keybindings :clojure
+    {:t {:category "toggles"
+         :p {:action "parinfer:toggleMode" :title "Toggle Parinfer Mode"}}})
   (println "init clojure"))
 
 (defmethod get-packages :lang/clojure
--- src/cljs/proton/lib/proton.cljs
+++ src/cljs/proton/lib/proton.cljs
@@ -1,7 +1,9 @@
 (ns proton.lib.proton
   (:require [cljs.reader :as reader]
             [cljs.nodejs :as node]
+            [proton.lib.mode :as mode-manager]
             [proton.lib.helpers :as helpers]
+            [proton.lib.atom :as atom-env]
             [proton.layers.base :as layerbase]))
 
 (def config-path (str (.. js/process -env -HOME) "/.proton"))
@@ -37,3 +39,10 @@
 
 (defn init-layers! [layers config]
   (doall (map #(layerbase/init-layer! (keyword %) config) layers)))
+
+(defn- on-active-pane-item [item]
+  (if-let [editor (atom-env/get-active-editor)]
+    (when (= (.-id editor) (.-id item))
+     (mode-manager/activate-mode editor))))
+
+(defn panel-item-subscription [] (.onDidChangeActivePaneItem atom-env/workspace on-active-pane-item))
