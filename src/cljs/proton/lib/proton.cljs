b2009b6e271d8b7fb0a9e687e40c464ae3027c5d
geksilla <buzhordenis@gmail.com>
2015-12-31 11:01:35 +0200

    Map cmd to proton:chain command.

--- src/cljs/proton/lib/proton.cljs
+++ src/cljs/proton/lib/proton.cljs
@@ -63,11 +63,11 @@
         command "proton:chain"
         make-char-fn (comp (partial map char) range)
         letters (make-char-fn 97 123)
-        numbers (map str (range 0 9))
+        numbers (map str (range 0 10))
         punctuation (flatten (map #(apply make-char-fn %) [[33 48] [58 65] [91 97] [123 127]]))
         special-chars ["escape" "tab" "backspace" "delete"]
         all-chars (into [] (concat [] letters (map string/upper-case letters) numbers special-chars punctuation))
-        combo ["ctrl-" "ctrl-alt-" "ctrl-shift-" "ctrl-shift-alt-" "alt-" "alt-shift-"]
+        combo ["ctrl-" "ctrl-alt-" "ctrl-shift-" "ctrl-shift-alt-" "alt-" "alt-shift-" "cmd-" "cmd-shift-" "cmd-alt-" "cmd-alt-shift-" "cmd-ctrl-" "cmd-ctrl-shift-"]
         all-combos (for [x all-chars y combo] (str y x))
         ret (flatten (concat [] all-chars all-combos))
         ret-map (reduce #(assoc %1 %2 command) {} (map identity ret))]
