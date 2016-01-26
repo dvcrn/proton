2225aba6ff558eeaca33b0cb3ca6373290590ead
geksilla <buzhordenis@gmail.com>
2016-01-26 21:26:01 +0200

    Fixed error on activation for windows, ref #70

--- src/cljs/proton/config/proton.cljs
+++ src/cljs/proton/config/proton.cljs
@@ -2,8 +2,8 @@
   (:require [cljs.nodejs :as node]))
 
 (def path (node/require "path"))
-(def user-home-dir (.normalize path (if (= (.-platform js/process) "win32") (.. js/process -env -USERPROFILE) (.. js/process -env -HOME))))
+(def user-home-dir (.homedir (node/require "os")))
 
-(def default {:config-path (str user-home-dir "/.proton")
+(def default {:config-path (.join path user-home-dir ".proton")
               :layers [:core]
               :debug false})
