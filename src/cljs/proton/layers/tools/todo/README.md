dbd727fdd578d2d6958b022927acb2f1b4ced162
David Mohl <me@dave.cx>
2016-02-05 16:30:18 +0900

    added todo layer

---
+++ src/cljs/proton/layers/tools/todo/README.md
@@ -0,0 +1,14 @@
+## Todo layer
+
+Integration for [todo-show](https://github.com/mrodalgaard/atom-todo-show) under `SPC a t`
+
+### Install
+
+Add `:tools/todo` to your layers.
+
+### Key Bindings
+
+Key Binding            | Description
+-----------------------|--------------------------------------
+<kbd> SPC a t p </kbd> | Find all todos in project / workspace
+<kbd> SPC a t b </kbd> | Find all todos in open buffers
---
+++ src/cljs/proton/layers/tools/todo/core.cljs
@@ -0,0 +1,24 @@
+(ns proton.layers.tools.todo.core
+  (:require [proton.lib.helpers :as helpers])
+  (:use [proton.layers.base :only [init-layer! get-initial-config get-keybindings get-packages get-keymaps describe-mode]]))
+
+(defmethod init-layer! :tools/todo
+  [_ {:keys [config layers]}]
+  (helpers/console! "init" :tools/todo))
+
+(defmethod get-keybindings :tools/todo
+  []
+  {:a {:category "apps"
+       :t {:category "todo"
+           :p {:action "todo-show:find-in-project"
+               :title "find in project"}
+           :b {:action "todo-show:find-in-open-files"
+               :title "find in buffers"}}}})
+
+(defmethod get-packages :tools/todo
+  []
+  [:todo-show])
+
+(defmethod get-keymaps :tools/todo [] [])
+(defmethod get-initial-config :tools/todo [] [])
+(defmethod describe-mode :tools/todo [] {})
