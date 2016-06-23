3e9081db0a3e578846810658dac4a9eaa7da919f
geksilla <buzhordenis@gmail.com>
2016-06-23 13:55:02 +0300

    feat(elixir): Added atom-elixir package and layer refactoring.
    * Added atom-elixir package to elixir layer.
    * Moved iex keybindings to `s` category.
    * Added more commands related to iex.
    * Removed autocomplete-elixr package.

--- src/cljs/proton/layers/lang/elixir/README.md
+++ src/cljs/proton/layers/lang/elixir/README.md
@@ -1,6 +1,12 @@
 ## Elixir configuration layer
 
-This layer adds support for elixir based files with full autocomplete support powered by [autocomplete-elixir](https://atom.io/packages/autocomplete-elixir).
+This layer adds support for elixir based files.
+
+Includes following atom packages:
+
+-  [language-elixir](https://atom.io/packages/language-elixir)
+-  [atom-elixir](https://atom.io/packages/atom-elixir)
+-  [iex](https://atom.io/packages/iex)
 
 ### Requirements
 Make sure that you have elixir (and iex) available on your system.
@@ -9,21 +15,20 @@ Make sure that you have elixir (and iex) available on your system.
 
 Add `:lang/elixir` to your layers.
 
-### Configuration
-
-Name                             | Default          | Type       | Description
----------------------------------|------------------|------------|-----------------------------------
-`autocomplete-elixir.erlangHome` | "/usr/local/bin" | __string__ | where is your `erl` located at?
-`autocomplete-elixir.elixirPath` | "/usr/local/bin" | __string__ | where is your `elixir` located at?
-
 ### Key Bindings
 
 | Key Binding          | Description                         |
 |----------------------|-------------------------------------|
-| <kbd>SPC m d t</kbd> | Toggle docs                         |
-| <kbd>SPC m d f</kbd> | Find selection in elixir docs       |
-| <kbd>SPC m i l</kbd> | Create iex shell on the right       |
-| <kbd>SPC m i h</kbd> | Create iex shell on the left        |
-| <kbd>SPC m i k</kbd> | Create iex shell above              |
-| <kbd>SPC m i j</kbd> | Create iex shell below              |
-| <kbd>SPC m i n</kbd> | Create iex shell in separate window |
+| <kbd>SPC m '</kbd>   | Run iex                             |
+| <kbd>SPC m g g</kbd> | Go to definition                    |
+| <kbd>SPC m g q</kbd> | Return from definition              |
+| <kbd>SPC m h h</kbd> | Find symbol in elixir docs          |
+| <kbd>SPC m s l</kbd> | Create iex shell on the right       |
+| <kbd>SPC m s h</kbd> | Create iex shell on the left        |
+| <kbd>SPC m s k</kbd> | Create iex shell above              |
+| <kbd>SPC m s j</kbd> | Create iex shell below              |
+| <kbd>SPC m s n</kbd> | Create iex shell in separate window |
+| <kbd>SPC m s g</kbd> | iex go to definition                |
+| <kbd>SPC m s e</kbd> | iex eval selection                  |
+| <kbd>SPC m s t</kbd> | iex run single test                 |
+| <kbd>SPC m s T</kbd> | iex run tests                       |
--- src/cljs/proton/layers/lang/elixir/core.cljs
+++ src/cljs/proton/layers/lang/elixir/core.cljs
@@ -1,7 +1,8 @@
 (ns proton.layers.lang.elixir.core
   (:require [proton.lib.helpers :as helpers]
+            [proton.lib.mode :as mode :refer [define-mode]]
             [proton.lib.atom :as atom-env :refer [set-grammar]])
-  (:use [proton.layers.base :only [init-layer! get-initial-config get-keybindings get-packages get-keymaps describe-mode register-layer-dependencies]]))
+  (:use [proton.layers.base :only [init-layer! get-initial-config get-keybindings get-packages get-keymaps describe-mode register-layer-dependencies init-package]]))
 
 (defmethod init-layer! :lang/elixir
   [_ {:keys [config layers]}]
@@ -11,30 +12,71 @@
 (defn- elixir-mode-init []
  (atom-env/set-grammar "Elixir"))
 
-(defmethod get-initial-config :lang/elixir []
-  [["autocomplete-elixir.erlangHome" "/usr/local/bin"]
-   ["autocomplete-elixir.elixirPath" "/usr/local/bin"]])
+(defmethod get-initial-config :lang/elixir [] [])
 
 (defmethod get-packages :lang/elixir []
   [:language-elixir
-   :autocomplete-elixir
+   :atom-elixir
    :iex])
 
+(defmethod init-package [:lang/elixir :atom-elixir] []
+  (helpers/console! "init atom-elixir package" :lang/elixir)
+  (mode/define-package-mode :atom-elixir
+    {:mode-keybindings
+      {:g {:category "go to"
+           :g {:action "atom-elixir:goto-definition"
+               :target "atom-text-editor:not(mini)"
+               :title "definition"}
+           :q {:action "atom-elixir:return-from-definition"
+               :target "atom-text-editor:not(mini)"
+               :title "return from definition"}}
+       :h {:category "help"
+           :h {:action "atom-elixir:show-elixir-docs"
+               :target "atom-text-editor:not(mini)"
+               :title "search at point"}}
+       :e {:category "eval/expand"
+           :m {:action "atom-elixir:expand-selected-text"
+               :target "atom-text-editor:not(mini)"
+               :title "expand macro"}
+           :q {:action "atom-elixir:quote-selected-text"
+               :target "atom-text-editor:not(mini)"
+               :title "quoted view"}}}})
+  (mode/link-modes :elixir-major-mode (mode/package-mode-name :atom-elixir)))
+
+(defmethod init-package [:lang/elixir :iex] []
+  (helpers/console! "init package iex" :lang/elixir)
+  (mode/define-package-mode :iex
+   {:mode-keybindings
+     {:s {:category "iex"
+          :l {:action "iex:open-split-right"
+              :title "create right"}
+          :h {:action "iex:open-split-left"
+              :title "create left"}
+          :k {:action "iex:open-split-up"
+              :title "create above"}
+          :j {:action "iex:open-split-down"
+              :title "create below"}
+          :n {:action "iex:open"
+              :title "new iex"}
+          :e {:action "iex:pipe"
+              :target "atom-text-editor:not(mini)"
+              :title "eval selection"}
+          :g {:action "iex:gotoDefinition"
+              :target "atom-text-editor"
+              :title "go to definition"}
+          :t {:action "iex:run-test"
+              :target "atom-text-editor"
+              :title "run test"}
+          :T {:action "iex:run-tests"
+              :target "atom-text-editor"
+              :title "run tests"}}
+      :quote {:action "iex:open-split-down"
+              :title "run iex"}}})
+  (mode/link-modes :elixir-major-mode (mode/package-mode-name :iex)))
+
 (defmethod describe-mode :lang/elixir []
-  {:mode-name :elixir
+  {:mode-name :elixir-major-mode
    :atom-grammars ["Elixir"]
-   :mode-keybindings
-   {:i {:category "iex"
-        :l {:action "iex:open-split-right"
-            :title "create right"}
-        :h {:action "iex:open-split-left"
-            :title "create left"}
-        :k {:action "iex:open-split-up"
-            :title "create above"}
-        :j {:action "iex:open-split-down"
-            :title "create below"}
-        :n {:action "iex:open"
-            :title "new iex"}}}
    :init elixir-mode-init})
 
 (defmethod get-keymaps :lang/elixir []
