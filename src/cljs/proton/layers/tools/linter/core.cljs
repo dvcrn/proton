887916a7d0867b5218e3edca808c3ceb01a0b29b
Denys Buzhor <buzhordenis@gmail.com>
2017-03-27 11:59:29 +0300

    fix(tools/linter) changed commands and packages according latest lint package (#262)
    * Linter v2 has following dependencies linter-ui-default, intentsions and busy-signal
     which will be removed since they are not included in tools/linter layer.
    * Updated keybinding actions

--- src/cljs/proton/layers/tools/linter/core.cljs
+++ src/cljs/proton/layers/tools/linter/core.cljs
@@ -1,29 +1,36 @@
 (ns proton.layers.tools.linter.core
   (:require [proton.lib.helpers :as helpers])
-  (:use [proton.layers.base :only [init-layer! get-initial-config get-keybindings get-packages get-keymaps describe-mode]]))
+  (:use [proton.layers.base :only [init-layer! get-keybindings get-packages get-keymaps describe-mode]]))
 
 (defmethod init-layer! :tools/linter
   [_ {:keys [config layers]}]
   (helpers/console! "init" :tools/linter))
 
 (defmethod get-packages :tools/linter []
-  [:linter])
+  [:linter-ui-default
+   :busy-signal
+   :intentions
+   :linter])
 
 (defmethod get-keybindings :tools/linter [] []
   {:e {:category "errors"
-       :t {:action "linter:toggle"
+       :t {:action "linter:toggle-active-editor"
            :title "toggle linter"}
-       :n {:action "linter:next-error"
+       :n {:action "linter-ui-default:next"
            :title "next error"}
-       :p {:action "linter:previous-error"
+       :p {:action "linter-ui-default:previous"
            :title "previous error"}
+       :N {:action "linter-ui-default:next-error-in-current-file"
+           :title "next error in current file"}
+       :P {:action "linter-ui-default:previous-error-in-current-file"
+           :title "previous error in current file"}
        :l {:action "linter:lint"
            :title "lint now"}
-       :T {:action "linter:togglePanel"
-           :title "toggle panel"}}})
+       :T {:action "linter-ui-default:toggle-panel"
+           :title "toggle panel"}
+       :f {:action "linter-ui-default:apply-all-solutions"
+           :title "apply all solutions"}}})
 
 
 (defmethod describe-mode :tools/linter [] {})
 (defmethod get-keymaps :tools/linter [] [])
-(defmethod get-initial-config :tools/linter []
-  [["linter.statusIconScope" "File"]])
